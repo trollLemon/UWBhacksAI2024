02930434042e77f8c8f43cfa13b0ee75de5a96c0
Haydn <trolllemon@protonmail.com>
2024-04-26 17:13:53 -0700

    endpoing for getting audio from user

--- uwbhacksai/demo/src/main/java/com/example/demo/GptAPI.java
+++ uwbhacksai/demo/src/main/java/com/example/demo/GptAPI.java
@@ -4,7 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.MediaType;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -37,7 +42,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
     }
 }
 
+class Audio{}
+
+
 @RestController
+@RequestMapping("/api")
 public class GptAPI {
 
 
@@ -46,7 +55,7 @@ public class GptAPI {
 		return "Greetings from Spring Boot!";
 	}
 
-	@PostMapping("/api/startTest")
+	@PostMapping("/startTest")
 	public String processRequest(@RequestBody Request request) {
         	
 	String tutorType = request.getTutorType();
@@ -54,6 +63,26 @@ public class GptAPI {
         
         
        	return "Request processed successfully";
-    }
+    	}
+
+	@PostMapping(value = "/sendStudentResponse", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public ResponseEntity<Resource> uploadFile(@RequestParam("file") MultipartFile file) {
+	//try {
+        //    byte[] data = audioService.processAudio(file);
+        //    Resource resource = new ByteArrayResource(data);
+
+        //    return ResponseEntity.ok()
+        //            .contentLength(data.length)
+        //            .contentType(MediaType.parseMediaType("audio/mpeg"))
+        //            .body(resource);
+       // } catch (Exception e) {
+         //   return ResponseEntity.internalServerError().build();
+        //}
+	//
+	return ResponseEntity.internalServerError().build();
+	}
+    
+
+
 
 }
