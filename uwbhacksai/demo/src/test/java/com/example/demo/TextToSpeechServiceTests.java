1396dcf49357d4f657ef6bf399dbddc8776dd0a5
Haydn <trolllemon@protonmail.com>
2024-04-27 11:32:47 -0700

    update tests

--- uwbhacksai/demo/src/test/java/com/example/demo/TextToSpeechServiceTests.java
+++ uwbhacksai/demo/src/test/java/com/example/demo/TextToSpeechServiceTests.java
@@ -28,7 +28,7 @@ class TextToSpeechServiceTest {
 
     @Test
     public void testTts() throws IOException {
-        String response = "Hello, world!";
+        String response = "Hello and welcome! Today, we're going to test your understanding of generative pre-trained transformers (GPT). I'll be asking you a series of questions to see how well you grasp the concepts we've discussed. Feel free to express your thoughts and reasoning as we go along. Are you ready to dive into the world of GPTs?Let's start with the first question:Can you explain what a generative pre-trained transformer (GPT) is and how it differs from other types of language models in the field of artificial intelligence?";
 
         // Calling the method to be tested
         instance.tts(response);
