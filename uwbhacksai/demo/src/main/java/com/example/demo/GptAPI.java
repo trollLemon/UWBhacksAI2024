f2d9c05ce6b92f92cd1948c96ebf966e307deb1b
Andres Lince <andres.lince@outlook.com>
2024-04-27 09:25:40 -0700

    Continue implementation of main endpoints

--- uwbhacksai/demo/src/main/java/com/example/demo/GptAPI.java
+++ uwbhacksai/demo/src/main/java/com/example/demo/GptAPI.java
@@ -4,7 +4,6 @@ import com.example.demo.tutor.SpeechToTextService;
 import com.example.demo.tutor.TextToSpeechService;
 import com.example.demo.tutor.TutorLLMService;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
