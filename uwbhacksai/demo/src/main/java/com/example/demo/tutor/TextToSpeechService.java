ce231802a890e1f19139ac42b0fa73902059e1e1
Haydn <trolllemon@protonmail.com>
2024-04-26 20:22:24 -0700

    better name

---
+++ uwbhacksai/demo/src/main/java/com/example/demo/tutor/TextToSpeechService.java
@@ -0,0 +1,54 @@
+package com.example.demo.tutor;
+
+import com.azure.ai.openai.OpenAIClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.ai.openai.models.SpeechGenerationOptions;
+import com.azure.ai.openai.models.SpeechVoice;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.BinaryData;
+import com.azure.core.util.Configuration;
+
+import org.springframework.stereotype.Service;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class TextToSpeechService{
+
+    private final OpenAIClient openAIClient;
+
+    public TextToSpeechService(OpenAIClient openAIClient) {
+        this.openAIClient = openAIClient;
+    }
+
+
+
+    public static void tts(String response) throws IOException {
+        String azureOpenaiKey =  Configuration.getGlobalConfiguration().get("AZURE_OPENAI_KEY");
+        String endpoint = Configuration.getGlobalConfiguration().get("AZURE_OPENAI_ENDPOINT");
+        String deploymentOrModelId = "tts";
+
+        OpenAIClient client = new OpenAIClientBuilder()
+                .endpoint(endpoint)
+                .credential(new AzureKeyCredential(azureOpenaiKey))
+                .buildClient();
+
+        SpeechGenerationOptions options = new SpeechGenerationOptions(
+                response,
+                SpeechVoice.ALLOY);
+
+        BinaryData speech = client.generateSpeechFromText(deploymentOrModelId, options);
+        // Checkout your generated speech in the file system.
+        Path path = Paths.get("./azure-ai-openai/src/samples/java/com/azure/ai/openai/resources/speech.wav");
+        Files.write(path, speech.toBytes());
+
+
+
+
+     }
+
+}
